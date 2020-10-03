c765cd8a5331705355e638b4a2251db57a83b829
HunterTigerX <HunterTigerX@gmail.com>
2020-10-03 03:55:01 +0300

    init: post solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0 ) { 
+    return Number(0);
 }
+else {
+  let minimumNumber = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < minimumNumber) {
+          minimumNumber = array[i];
+          }
+        }
+            return minimumNumber;
+          }}
 
+          
 exports.max = function max (array) {
-  return 0;
-}
+    if (array == undefined || array.length == 0 ) { 
+      return Number(0);
+  }
+  else {
+    let maximumNumber = array[0];
+      for (var i = 1; i < array.length; i++) {
+          if (array[i] > maximumNumber) {
+            maximumNumber = array[i];
+            }
+          }
+              return maximumNumber;
+            }}
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0 ) { 
+    return Number(0);
 }
+else {
+    let arraySum = array[0];
+    for (var i = 1; i < array.length; i++) {
+      arraySum = array[i] + arraySum;
+    }
+    let arrayAvg = arraySum / (i);
+    return arrayAvg;
+    }}
